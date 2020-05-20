0bbff2c6668093648d9ebc02ac446a2353ef7a7b
sajjadkareem <63907914+sajjadwar199@users.noreply.github.com>
2020-05-20 12:14:20 +0300

    Update database.php

--- database.php
+++ database.php
@@ -76,7 +76,7 @@ public static function update($table,$where,$set,$successmsg=null){
 } 
     
 
-public static function delete($table,$idname,$id,$successmsg){
+public static function delete($table,$idname,$id,$successmsg=null){
 
    
         $query = "DELETE FROM $table WHERE $idname = $id";
@@ -84,12 +84,13 @@ public static function delete($table,$idname,$id,$successmsg){
     $conn = $c->connect();
       $q= mysqli_query($conn,$query);
        if($q){
+       
+        $_SESSION['success_message']=$successmsg;
         echo '     <script>
         if ( window.history.replaceState ) {
           window.history.replaceState( null, null, window.location.href );
         }
         </script>';
-        $_SESSION['success_message']=$successmsg;
       }else{
         $_SESSION['error_massage'] = 'هناك خطا  ';
     
@@ -126,6 +127,69 @@ if ($q == false) {
 $num=mysqli_num_rows($q);
 return $num;
 }  
+
+
+
+function query($sql)
+{
+    $c = new main;
+    $conn = $c->connect();
+    $query = mysqli_query($conn, $sql);
+    if (!$query) {
+
+        $_SESSION['error_massage'] = 'åäÇß ÎØÇ Ýí ÇáÇÓÊÚáÇã';
+    } else {
+        echo "";
+    }
+    return $query;
+}
+//fetch array of all information from database 
+public static function  fetch($q)
+{
+    $row = mysqli_fetch_array($q);
+    return $row;
+}
+
+public static function model($sql2, $alert = null, $page = null)
+{
+    $c = new main;
+    $conn = $c->connect();
+    @$query = mysqli_query($conn, $sql2);
+    if ($query and $alert != "" and $page != "") {
+        echo "<script>alert('$alert')</script>";
+        echo "<Script>window.open('$page','_self')</Script>";
+    } else if ($alert != "" and $page != "") {
+        echo "<script>alert('åäÇß ÎØÇ ')</script>";
+        echo "<Script>window.open('$page','_self')</Script>";
+    }
+}
+public  function insert_last_id($table_name,$assoc_array,$successmsg=null){
+
+  $keys = array();
+  $values= array();
+  foreach($assoc_array as $key => $value){
+      $keys[] = $key;
+      $values[] =$this->escape_string(strip_tags(htmlentities($_POST[$value]))) ;
+  }
+  $query = "INSERT INTO `$table_name`(`".implode("`,`", $keys)."`) VALUES('".implode("','", $values)."')";
+  $c = new database;
+  $conn = $c->connect();
+  $q=  mysqli_query($conn,$query);
+  if($q){
+    $last=$conn->insert_id;
+  $_SESSION['success_message']=$successmsg.$last;
+  echo  '     <script>
+  if ( window.history.replaceState ) {
+    window.history.replaceState( null, null, window.location.href );
+  }
+  </script>';
+}else{
+  $_SESSION['error_massage'] = 'هناك خطا  ';
+
+}
+} 
+
+
 
 };
 
