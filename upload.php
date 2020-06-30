51666d6d6cb36c4a79bb043c8d7d6dc0fe5a1dfa
sajjadkareem <63907914+sajjadwar199@users.noreply.github.com>
2020-06-30 07:41:10 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,29 +2,33 @@
 require 'classes/pagination.php' ;
 class upload extends pagination{
 
- public static function upload_image($image, $file_upload_to, $page)
+    function upload_image($image, $file_upload_to, $page)
     {
-        $rand = substr(md5(uniqid(rand(), true)), 3, 10);
-        if (isset($image)) {
+
+        $rand = substr(md5(uniqid(rand(), true)), 3, 10); //create md5 for file
+        if (isset($image)) { //isset of image name from form
             $image = $image;
         }
-        $file_name = $_FILES["$image"]["name"];
-        $file_tmp_name = $_FILES["$image"]["tmp_name"];
-        $file_size = $_FILES["$image"]["size"];
-        $file_type = $_FILES["$image"]["type"];
-        $file_error = $_FILES["$image"]["error"];
-        $folder = "$file_upload_to /";
-        $path = $folder . $rand . $file_name;
-        $tmp = explode('.', $file_name);
-        $test_type = end($tmp);
-        $type = array('jpeg', 'jpg', 'png', 'gif', 'jfif');
 
-        if (in_array($test_type, $type) and $file_size < 1000000) {
-            move_uploaded_file($file_tmp_name, $path);
+        $file_name = $_FILES["$image"]["name"]; //  1-get the name of file 
+        $file_tmp_name = $_FILES["$image"]["tmp_name"]; // 2-get the temb_name of file
+        $file_size = $_FILES["$image"]["size"]; // 3-get size of file 
+        $file_type = $_FILES["$image"]["type"]; // 4-get the type of file
+        $file_error = $_FILES["$image"]["error"]; //5-get any error
+        $folder = "$file_upload_to/"; //6- the dir of upload folder
+        $path = $folder.$rand.$file_name; //7-make path of file 
+        $tmp = explode('.', $file_name); //8-search from allow types of file
+        $test_type = strtolower(end($tmp));
+        $type = array('jpeg', 'jpg', 'png', 'gif', 'jfif');
+        $check = getimagesize($file_tmp_name);
+        if (in_array($test_type, $type) and $file_size < 2000000 and $check!=false) {
+            move_uploaded_file($file_tmp_name, $path); //upload file to folder
             return $path;
         } else {
-            echo "<script>alert(' لرجاء اضافة صورة بصيغة مدعومة اقل من 1 ميغا بايت!لم تتم اضافة ')</script>";
-            echo "<Script>window.open('$page','_self')</Script>";
+           
+           echo "<script>alert(' لرجاء اضافة صورة بصيغة مدعومة اقل من 2 ميغا بايت!لم تتم اضافة ')</script>";
+            echo "<Script>window.open('$page','_self')</Script>"; //if found any error 
+            return $path=false;
         }
     }
 
@@ -45,13 +49,16 @@ class upload extends pagination{
     $tmp = explode('.', $file_name);
     $test_type = end($tmp);
     $type = array('pdf');
-
-    if (in_array($test_type, $type) and $file_size < 3000000) {
-        move_uploaded_file($file_tmp_name, $path);
+    //chack the image is the fake or not
+    $check = getimagesize($file_tmp_name);
+    if (in_array($test_type, $type) and $file_size < 2000000 and $check!=false) {
+        move_uploaded_file($file_tmp_name, $path); //upload file to folder
         return $path;
     } else {
-        echo "<script>alert(' لرجاء اضافة ملف  نصي بصيغة مدعومة اقل من 3 ميغا بايت!لم تتم اضافة ')</script>";
-        echo "<Script>window.open('$page','_self')</Script>";
+       
+       echo "<script>alert(' لرجاء اضافة صورة بصيغة مدعومة اقل من 2 ميغا بايت!لم تتم اضافة ')</script>";
+        echo "<Script>window.open('$page','_self')</Script>"; //if found any error 
+        return $path=false;
     }
 }
 };
