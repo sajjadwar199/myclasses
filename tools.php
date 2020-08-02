512a05ccf004b8561fbc786a5ae482eb9b8059c3
sajjadkareem <63907914+sajjadwar199@users.noreply.github.com>
2020-08-02 13:13:10 +0300

    Update tools.php

--- tools.php
+++ tools.php
@@ -1,16 +1,16 @@
 <?php  
-require 'classes/time.php' ;
+require 'time.php' ;
 class tools extends time {
 
       //  دالة توليد ملفات الرفع اوتماتيكيا
-      public static   function create_upload_file($folder_name)
-      {
+    public static   function create_upload_file($folder_name)
+    {
           if (!is_dir($folder_name)) {
               mkdir($folder_name);
           }
       }
 
-      public static function delete_file($url)
+    public static function delete_file($url)
     {
 
         $filename = dirname(__FILE__) . "$url";
@@ -22,6 +22,28 @@ class tools extends time {
     {
         return header('location:' . $page . '.php');
     }
+    public static function redir_inc($page_include){
+        if($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] == $_SERVER['HTTP_HOST'].'/pos/views'.$page_include){
+            header('location:404') ;
+        
+        }
+    }
 
+public function bootstrab_header(){
+    echo '<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap CRUD Data Table for Database with Modal Form</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>';
+    }
+public function bootstrab_footer(){
+
+    }
+   
 }
 ?>
